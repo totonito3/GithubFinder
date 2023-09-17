a527ee4d65a6c58badc6aee96a39a39683961a3f
Bertin Tamegnon <btamegnon3@gmail.com>
2023-09-17 18:40:17 -0400

    Initial commit.Worked on various components and alertContext GithubContext and Reducers

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,58 @@
+import { createContext, useReducer, useEffect } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const { items } = await res.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+    //setUsers(data);
+    // setLoading(false);
+  };
+
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
+  const setLoading = () => {
+    dispatch({
+      type: "SET_LOADING",
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserSearch from "../components/users/UserSearch";
+import UserResult from "../components/users/UserResult";
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
